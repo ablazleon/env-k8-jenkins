ee8313da9c627c701f66bae0f1c2470d2c69a4f6
ablazleon <ablazleon@gmail.com>
2020-06-22 18:10:26 +0200

    eks try

---
+++ eks/main.tf
@@ -0,0 +1,13 @@
+module "eks-jx" {
+  source  = "jenkins-x/eks-jx/aws"
+}
+
+output "vault_user_id" {
+  value       = module.eks-jx.vault_user_id
+  description = "The Vault IAM user id"
+}
+
+output "vault_user_secret" {
+  value       = module.eks-jx.vault_user_secret
+  description = "The Vault IAM user secret"
+}
